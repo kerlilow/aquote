aeaf2eca1662e9232537a371a889cdb40d21018e
Kerli Low <kerlilow@gmail.com>
2021-03-18 12:33:33 +0900

    Improvise install script

--- platform/linux/install.sh
+++ platform/linux/install.sh
@@ -1,14 +1,24 @@
 #!/bin/sh
 
-# Install binary
-cp aquote /usr/local/bin/
+error_exit() {
+    echo "Failed to install aquote"
+    exit 1
+}
 
-# Initialize config
-mkdir /etc/aquote
-cp config/default.toml /etc/aquote/config.toml
+# # Initialize config
+mkdir -p /etc/aquote || error_exit
+cp config/default.toml /etc/aquote/config.toml || error_exit
 
-# Add, enable, and start scheduled job
-cp systemd/* /etc/systemd/system/
-systemctl daemon-reload
-systemctl enable aquote-fetch.timer
-systemctl start aquote-fetch.timer
+# # Install binary
+cp aquote /usr/local/bin/ || error_exit
+
+# # Add, enable, and start scheduled job
+cp systemd/* /etc/systemd/system/ || error_exit
+systemctl daemon-reload || error_exit
+systemctl enable aquote-fetch.timer || error_exit
+systemctl start aquote-fetch.timer || error_exit
+
+echo "Successfully installed aquote"
+
+echo "Add \"aquote show\" to your .bashrc, .zshrc, fish_greeting.fish, etc. \
+to display the quote of the day when you start your shell"
--- platform/macos/install.sh
+++ platform/macos/install.sh
@@ -1,11 +1,22 @@
 #!/bin/sh
 
-# Install binary
-cp aquote /usr/local/bin/
+error_exit() {
+    echo "Failed to install aquote"
+    exit 1
+}
 
 # Initialize config
-mkdir /etc/aquote
-cp config/default.toml /etc/aquote/config.toml
+mkdir -p /etc/aquote || error_exit
+cp config/default.toml /etc/aquote/config.toml || error_exit
 
-# Add scheduled job
-cp launchd/me.kerlilow.aquote.fetch.plist /Library/LaunchDaemons/
+# Install binary
+cp aquote /usr/local/bin/ || error_exit
+
+# Add and load scheduled job
+cp launchd/me.kerlilow.aquote.fetch.plist /Library/LaunchDaemons/ || error_exit
+launchctl load -w /Library/LaunchDaemons/me.kerlilow.aquote.fetch.plist || error_exit
+
+echo "Successfully installed aquote"
+
+echo "Add \"aquote show\" to your .bashrc, .zshrc, fish_greeting.fish, etc. \
+to display the quote of the day when you start your shell"
