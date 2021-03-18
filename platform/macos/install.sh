26cec70d8ef8e00ae1d53f4abebc6cb065fc761b
Kerli Low <kerlilow@gmail.com>
2021-03-18 11:02:18 +0900

    Add trailing slash

--- platform/macos/install.sh
+++ platform/macos/install.sh
@@ -8,4 +8,4 @@ mkdir /etc/aquote
 cp config/default.toml /etc/aquote/config.toml
 
 # Add scheduled job
-cp launchd/me.kerlilow.aquote.fetch.plist /Library/LaunchDaemons
+cp launchd/me.kerlilow.aquote.fetch.plist /Library/LaunchDaemons/
